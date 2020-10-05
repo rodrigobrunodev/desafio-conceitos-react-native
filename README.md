fe7a5f19a785a2bf78c4a79bdbe47baf85f29669
Rodrigo Souza <ro_web1@hotmail.com>
2020-10-05 19:55:46 -0300

    finalizando desafio para listar e curtir repositoórios

---
+++ README.md
@@ -0,0 +1,7 @@
+npm start
+# Ou yarn start
+
+npx react-native run-android
+
+# Ou yarn run react-native run-android
+# Ou yarn android
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:3333",
+export const api = axios.create({
+  baseURL: "http://192.168.15.16:3333",
 });
 
-export default api;
